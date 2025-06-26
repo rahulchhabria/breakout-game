1683869ee58906afed7a6f981a01394616c087c5
Rahul Chhabria <rahul.chhabria@sentry.io>
2025-06-26 00:42:53 -0700

    fixed sentry integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true, // Required for Sentry source maps
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'rc-sentry-projects',
+      project: '4509563503640576',
+      authToken: process.env.SENTRY_AUTH_TOKEN, // Set this in your environment
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
