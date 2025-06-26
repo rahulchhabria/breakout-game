a7110b6f02829aff4873fb0d662ed804e4b08a8d
Rahul Chhabria <rahul.chhabria@sentry.io>
2025-06-26 09:20:49 -0700

    sentry tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
+// Import Sentry instrumentation first
+import './sentry';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: 'https://fff74e528b4cafe486546e7e9898d710@o4506312335294464.ingest.us.sentry.io/4509563503640576',
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 1.0, // Adjust in production as needed
-  replaysSessionSampleRate: 1.0, // Capture 100% of all sessions for testing
-  replaysOnErrorSampleRate: 1.0, // 100% of sessions with errors
-  _experiments: { enableLogs: true }, // Enable Sentry logs
-});
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>} showDialog>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Sentry.ErrorBoundary>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 /// <reference types="node" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin, type SentryVitePluginOptions } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'rc-sentry-projects',
+      project: 'breakout-game',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        assets: './dist',
+      },
+    } satisfies SentryVitePluginOptions),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    sourcemap: true,
   },
 });
